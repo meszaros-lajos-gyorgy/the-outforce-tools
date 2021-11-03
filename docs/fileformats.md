f8e797027cca12b479e77ada7541e77acabe94dd
Mészáros Lajos <m_lajos@hotmail.com>
2021-11-03 20:19:05 +0100

    The box poc can now read the manifest of the files

--- docs/fileformats.md
+++ docs/fileformats.md
@@ -4,15 +4,13 @@
 
 `*.box`
 
-// TODO: copy specs from watto's page
+http://www.watto.org/specs.html?specs=Archive_BOX
+
+The last 4 bytes of the file determnine when the directory info starts.
+It will probably never will be an issue, but those 4 bytes/32 bits create an upper bound to how large a box file can be: 4 GB.
 
 ## opf
 
 `*.opf`
 
-// TODO: copy specs from watto's page
-
-### Sources:
-
-- http://www.watto.org/specs.html?specs=Archive_BOX
-- http://www.watto.org/specs.html?specs=Archive_OPF_OUTFORCE
+http://www.watto.org/specs.html?specs=Archive_OPF_OUTFORCE
--- poc/box/index.js
+++ poc/box/index.js
@@ -1,9 +1,34 @@
 const fs = require("fs");
 const path = require("path");
 const { DEFAULT_INSTALL_DIR } = require("../../src/constants");
+require("stringview");
+
+const file = "Music.box";
 
 (async () => {
-  const src = path.resolve(DEFAULT_INSTALL_DIR, "./Outforce/Music.box");
+  const src = path.resolve(DEFAULT_INSTALL_DIR, "./Outforce/", file);
   const box = await fs.promises.readFile(src);
-  console.log(box.length);
+
+  const io = new DataView(box.buffer);
+
+  const directoryOffset = io.getUint32(io.byteLength - 4, true);
+  const numberOfFiles = io.getUint32(directoryOffset, true);
+
+  const files = [];
+
+  let cursor = directoryOffset + 4;
+  for (let i = 0; i < numberOfFiles; i++) {
+    const filename = io.getStringNT(cursor);
+    cursor += filename.length + 1;
+    files.push({
+      filename,
+      offset: io.getUint32(cursor, true),
+      size: io.getUint32(cursor + 4, true),
+    });
+    cursor += 8;
+  }
+
+  console.log("           file:", file);
+  console.log("number of files:", numberOfFiles);
+  console.log(files);
 })();
