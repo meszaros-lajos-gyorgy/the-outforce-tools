3a42f9b30f1858473a3db525a8a3c6ffd09bf017
Mészáros Lajos <m_lajos@hotmail.com>
2021-11-04 03:22:25 +0100

    small fix + refactor for manifest reader part + files are now being saved to disk

--- poc/box/index.js
+++ poc/box/index.js
@@ -1,17 +1,41 @@
 const fs = require("fs");
 const path = require("path");
+const {
+  pluck,
+  compose,
+  map,
+  split,
+  init,
+  join,
+  uniq,
+  reject,
+  isEmpty,
+} = require("ramda");
 const { DEFAULT_INSTALL_DIR } = require("../../src/constants");
 require("stringview");
 
-const file = "Music.box";
+// source: https://gist.github.com/miguelmota/5b06ae5698877322d0ca#gistcomment-3611597
+const toArrayBuffer = (buffer) => {
+  return buffer.buffer.slice(
+    buffer.byteOffset,
+    buffer.byteOffset + buffer.byteLength
+  );
+};
 
-(async () => {
-  const src = path.resolve(DEFAULT_INSTALL_DIR, "./Outforce/", file);
+const openBox = async (file) => {
+  const src = path.resolve(DEFAULT_INSTALL_DIR, `./Outforce/${file}.box`);
+
+  try {
+    await fs.promises.access(src, fs.constants.R_OK);
+  } catch (e) {
+    console.error("file not found:", src);
+    return;
+  }
+
   const box = await fs.promises.readFile(src);
+  const io = new DataView(toArrayBuffer(box));
 
-  const io = new DataView(box.buffer);
-
-  const directoryOffset = io.getUint32(io.byteLength - 4, true);
+  const directoryOffset = io.getUint32(box.length - 4, true);
   const numberOfFiles = io.getUint32(directoryOffset, true);
 
   const files = [];
@@ -19,16 +43,46 @@ const file = "Music.box";
   let cursor = directoryOffset + 4;
   for (let i = 0; i < numberOfFiles; i++) {
     const filename = io.getStringNT(cursor);
-    cursor += filename.length + 1;
+    const offset = io.getUint32(cursor + filename.length + 1, true);
+    const size = io.getUint32(cursor + filename.length + 5, true);
+    cursor += filename.length + 9;
     files.push({
-      filename,
-      offset: io.getUint32(cursor, true),
-      size: io.getUint32(cursor + 4, true),
+      path: filename.replace("\\", "/"),
+      data: box.slice(offset, offset + size),
     });
-    cursor += 8;
   }
 
-  console.log("           file:", file);
-  console.log("number of files:", numberOfFiles);
-  console.log(files);
+  return files;
+};
+
+const getFolders = (files) => {
+  return compose(
+    reject(isEmpty),
+    uniq,
+    map(compose(join("/"), init, split("/"))),
+    pluck("path")
+  )(files);
+};
+
+(async () => {
+  const boxname = "Music";
+  const files = await openBox(boxname);
+
+  // const outputDir = path.resolve(DEFAULT_INSTALL_DIR, "./unpacked/boxes/");
+  const outputDir = path.resolve("e:/the-outforce-boxes/");
+
+  const folders = getFolders(files);
+
+  for (let folder of folders) {
+    await fs.promises.mkdir(path.resolve(outputDir, `${boxname}.box`, folder), {
+      recursive: true,
+    });
+  }
+
+  for (let file of files) {
+    await fs.promises.writeFile(
+      path.resolve(outputDir, `${boxname}.box`, file.path),
+      file.data
+    );
+  }
 })();
